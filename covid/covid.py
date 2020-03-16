d34b07890401ab57b00a28e4fd65404fd5f48c16
Jacek Chmielewski <jchmielewski@teonite.com>
2020-03-16 05:44:15 +0100

    API changes, use csvs.

--- covid/covid.py
+++ covid/covid.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python
 
 import sys
-from typing import List, Dict
+from collections import defaultdict
+from io import StringIO
+from typing import List
 import requests
+import csv
 from colorama import Fore, Style
 
-
-DATA_URL = "https://services9.arcgis.com/N9p5hsImWXAccRNI/arcgis/rest/services/Z7biAeD8PAkqgmWhxG2A/FeatureServer/2/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc&resultOffset=0&resultRecordCount=200&cacheHint=true"
-ATTRIBUTES = ['Country_Region', 'Confirmed', 'Active', 'Deaths', 'Recovered']
+DATA_URL_TEMPLATE = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-{}.csv'
+COUNTRY_COLUMN = 'Country/Region'
 SPACING = 10
-
-
-def print_country_stats(country_data: Dict):
-    for attribute in ATTRIBUTES:
-        if attribute == 'Country_Region':
-            print(Style.BRIGHT + Fore.GREEN, country_data[attribute], Style.RESET_ALL)
-        else:
-            print(Style.BRIGHT, attribute, Style.RESET_ALL, ' ' * (SPACING - len(attribute)), country_data[attribute])
 
 
 def parse_args(argv: List[str]):
@@ -27,17 +21,57 @@ def parse_args(argv: List[str]):
     return [c.lower() for c in countries]
 
 
+def accumulate(csv_rows, countries: List[str], country_column: str):
+    # sum cases from csv_rows in each country present in countries list accumulating its provinces/states.
+    result = defaultdict(int)
+    for row in [r for r in csv_rows if r[country_column].lower() in countries]:  # filter out rows from other countries
+        result[row[country_column]] += int(row[-1])  # get last day from timeseries (latest datapoint)
+
+    return result
+
+
 if __name__ == '__main__':
-    get_country_name = lambda cdict: cdict['attributes']['Country_Region'].lower()
+
     countries = parse_args(sys.argv)
-    data = requests.get(DATA_URL).json()['features']
-    countries_data = {
-        get_country_name(country): {
-            attribute: country['attributes'][attribute] for attribute in ATTRIBUTES
-        } for country in data if get_country_name(country) in countries
-    }
+    confirmed, deaths, recovered = [
+        csv.reader(
+            StringIO(requests.get(DATA_URL_TEMPLATE.format(dataset)).text), delimiter=','
+        )
+        for dataset in ['Confirmed', 'Deaths', 'Recovered']
+    ]
+    confirmed_columns, deaths_columns, recovered_columns = next(confirmed), next(deaths), next(recovered)
+    confirmed_country_index, deaths_country_index, recovered_country_index = (
+        confirmed_columns.index(COUNTRY_COLUMN),
+        deaths_columns.index(COUNTRY_COLUMN),
+        recovered_columns.index(COUNTRY_COLUMN)
+    )
+
+    confirmed_results = accumulate(confirmed, countries, confirmed_country_index)
+    deaths_results = accumulate(deaths, countries, deaths_country_index)
+    recovered_results = accumulate(recovered, countries, recovered_country_index)
 
     print()
-    for country in countries:
-        print_country_stats(countries_data[country])
+    for country in confirmed_results.keys():
+        print(Style.BRIGHT + Fore.GREEN, country, Style.RESET_ALL)
+        print(
+            Style.BRIGHT,
+            'Confirmed:',
+            Style.RESET_ALL,
+            ' ' * (SPACING - len('Confirmed')),
+            confirmed_results[country]
+        )
+        print(
+            Style.BRIGHT,
+            'Deaths:',
+            Style.RESET_ALL,
+            ' ' * (SPACING - len('Deaths')),
+            deaths_results[country]
+        )
+        print(
+            Style.BRIGHT,
+            'Recovered:',
+            Style.RESET_ALL,
+            ' ' * (SPACING - len('Recovered')),
+            recovered_results[country]
+        )
         print()
